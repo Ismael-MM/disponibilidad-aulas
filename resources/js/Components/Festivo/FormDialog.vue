c051ca21cb2692a2a08aed5d7cdf065ff8a4ccab
Ismael Montelongo Montelongo <ismontelongo@outlook.com>
2024-03-18 10:51:04 +0000

    add: front + crud para festivos

---
+++ resources/js/Components/Festivo/FormDialog.vue
@@ -0,0 +1,119 @@
+<script setup>
+import { useForm } from "@inertiajs/vue3"
+import { computed, watch, ref } from "vue"
+import {
+  ruleRequired,
+  ruleMaxLength,
+} from "@/Utils/rules"
+import { sexoItems } from "@/Utils/arrays"
+
+const props = defineProps(["show", "item", "type", "endPoint"])
+const emit = defineEmits(["closeDialog", "reloadItems"])
+
+const dialogState = computed({
+  get: () => props.show,
+  set: (value) => {
+    emit("closeDialog", value)
+  },
+})
+
+const form = ref(false)
+
+const formData = useForm({
+  nombre: "",
+  fecha: "",
+})
+
+watch(dialogState, (value) => {
+  if (value) {
+    if (props.type === "edit") {
+      Object.assign(formData, props.item)
+    } else if (props.type === "create") {
+      formData.nombre = ""
+      formData.fecha = ""
+    }
+  } else {
+    emit("reloadItems")
+  }
+})
+
+const submit = () => {
+  if (props.type === "edit") {
+    formData.put(`${props.endPoint}/${props.item.id}`, {
+      only: ["tableData", "flash", "errors"],
+      onSuccess: () => {
+        dialogState.value = false
+      },
+    })
+  } else if (props.type === "create") {
+    formData.post(props.endPoint, {
+      only: ["tableData", "flash", "errors"],
+      onSuccess: () => {
+        dialogState.value = false
+      },
+    })
+  }
+}
+</script>
+
+<template>
+  <v-dialog v-model="dialogState" width="1024">
+    <v-card>
+      <v-card-title>
+        <span class="text-h5"
+          >{{
+            props.type == "create"
+              ? "Crear"
+              : props.type == "edit"
+              ? "Editar"
+              : ""
+          }}
+          Festivos</span
+        >
+      </v-card-title>
+
+      <v-card-text>
+        <v-container>
+          <v-form v-model="form" @submit.prevent="submit">
+            <v-row>
+              <v-col cols="12" sm="6">
+                <v-text-field
+                  label="Nombre*"
+                  :rules="[ruleRequired, (v) => ruleMaxLength(v, 191)]"
+                  v-model="formData.nombre"
+                ></v-text-field>
+              </v-col>
+              <v-col cols="12" sm="6">
+                <v-text-field
+                  label="Fecha*"
+                  type="date"
+                  :rules="[ruleRequired]"
+                  v-model="formData.fecha"
+                ></v-text-field>
+              </v-col>
+            </v-row>
+          </v-form>
+        </v-container>
+      </v-card-text>
+
+      <v-card-actions>
+        <v-spacer></v-spacer>
+        <v-btn
+          color="blue-darken-1"
+          variant="text"
+          @click="dialogState = false"
+        >
+          Cerrar
+        </v-btn>
+        <v-btn
+          color="blue-darken-1"
+          :disabled="!form"
+          variant="text"
+          @click="submit"
+        >
+          Guardar
+        </v-btn>
+      </v-card-actions>
+    </v-card>
+  </v-dialog>
+</template>
---
+++ resources/js/Utils/routes.js
@@ -0,0 +1,96 @@
+import { computed } from "vue"
+
+/*
+ * Parent routes will have "path" parameters instead or "route" ones
+ * "routes" will be used in their childs
+ */
+
+export const routesArray = [
+  {
+    value: "Usuario",
+    icon: "mdi-account-circle",
+  },
+  {
+    value: "Suscriptores",
+    route: "dashboard.suscriptores",
+    icon: "mdi-account-group",
+  },
+  {
+    value: "Cursos",
+    route: "dashboard.cursos",
+    icon: "mdi-school",
+  },
+  {
+    value: "Sedes",
+    route: "dashboard.sedes",
+    icon: "mdi-home-group",
+  },
+  {
+    value: "Aulas",
+    route: "dashboard.aulas",
+    icon: "mdi-google-classroom",
+  },
+  {
+    value: "Reservar aula",
+    route: "dashboard.reservar",
+    icon: "mdi-calendar-clock",
+  },
+  {
+    value: "Festivos",
+    route: "dashboard.festivo",
+    icon: "mdi-calendar-multiselect",
+  },
+  {
+    value: "Calendario",
+    route: "dashboard.calendario",
+    icon: "mdi-calendar-month",
+  },
+  {
+    value: "Cerrar sesión",
+    icon: "mdi-logout-variant",
+    route: "logout",
+  },
+  // Route with childs example:
+  /*
+  {
+    value: "title",
+    icon: "icon",
+    path: "relative-path",
+    childs: [
+      {
+        value: "title",
+        route: "route name",
+        icon: "icon",
+      },
+      {
+        value: "title",
+        route: "route name",
+        icon: "icon",
+      },
+    ],
+  },
+  */
+]
+
+export const routes = computed({
+  get() {
+    return routesArray
+  },
+  set({
+    newValue,
+    element = 1,
+    key = "value",
+    child = false,
+    childElement = 1,
+  }) {
+    let handler = routesArray
+
+    if (child) {
+      handler[element - 1].childs[childElement - 1][key] = newValue
+    } else {
+      handler[element - 1][key] = newValue
+    }
+
+    return handler
+  },
+})
