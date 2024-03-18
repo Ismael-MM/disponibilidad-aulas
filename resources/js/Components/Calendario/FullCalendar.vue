94a918a0a8620be87664b1be39bbf88dbc1e54a5
Ismael Montelongo Montelongo <ismontelongo@outlook.com>
2024-03-18 14:54:38 +0000

    update: calendario

---
+++ resources/js/Components/Calendario/FullCalendar.vue
@@ -0,0 +1,19 @@
+<script>
+import { onBeforeMount } from "vue"
+import FullCalendar from '@fullcalendar/vue3'
+import dayGridPlugin from '@fullcalendar/daygrid'
+import esLocale from '@fullcalendar/core/locales/es';
+
+export default {
+    components: {
+        FullCalendar // make the <FullCalendar> tag available
+    },
+};
+
+
+onBeforeMount(() => { })
+</script>
+
+<template>
+    <FullCalendar/>
+</template>
--- resources/js/Pages/Dashboard/Calendario.vue
+++ resources/js/Pages/Dashboard/Calendario.vue
@@ -1,37 +1,37 @@
-<script>
-import { onBeforeMount } from "vue"
-import FullCalendar from '@fullcalendar/vue3'
-import dayGridPlugin from '@fullcalendar/daygrid'
+<script setup>
+import {ref, onBeforeMount } from "vue"
+import FullCalendar from "@/Components/Calendario/FullCalendar.vue";
+import dayGridPlugin from '@fullcalendar/daygrid';
 import esLocale from '@fullcalendar/core/locales/es';
 
-export default {
-    components: {
-        FullCalendar // make the <FullCalendar> tag available
-    },
-    data: function () {
-        return {
-            calendarOptions: {
-                plugins: [dayGridPlugin],
-                locale: esLocale,
-                initialView: 'dayGridMonth',
-                weekends: false,
-                events: [
-                    { title: 'Meeting', start: new Date() },
-                    {
-                        title: 'Curso x aula 2',
-                        start: '2024-03-05',
-                        end: '2024-03-22',
-                    },
-                    { title: 'event 3', date: '2024-04-02' },
-                ],
-            }
-        }
-    }
-}
+const calendarOptions = {
+    plugins: [dayGridPlugin],
+    locale: esLocale,
+    initialView: 'dayGridMonth',
+    weekends: false,
+    events: [
+        { title: 'Meeting', start: new Date() },
+        {
+            title: 'Curso x aula 2',
+            start: '2024-03-05',
+            end: '2024-03-22',
+        },
+        { title: 'event 3', date: '2024-04-02' },
+        { title: 'event 6', date: '2024-04-02' },
+        { title: 'event 8', date: '2024-04-02' },
+    ],
+};
 
-onBeforeMount(() => { })
+
+onBeforeMount(() => {
+    
+})
 </script>
 
 <template>
-    <FullCalendar :options='calendarOptions' />
+    <v-card elevation="6" class="ma-5" variant="outlined">
+        <FullCalendar
+        :options='calendarOptions'
+        ></FullCalendar>
+    </v-card>
 </template>
