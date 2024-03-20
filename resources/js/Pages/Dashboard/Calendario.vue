a415239833ca4bb88d56dc36805b3dcd161a9bc0
Ismael Montelongo Montelongo <ismontelongo@outlook.com>
2024-03-20 11:06:10 +0000

    add: funcionalidad para cargar datos del turno de mañana o tarde

--- resources/js/Pages/Dashboard/Calendario.vue
+++ resources/js/Pages/Dashboard/Calendario.vue
@@ -4,8 +4,32 @@ import FullCalendar from "@/Components/Calendario/FullCalendar.vue";
 import dayGridPlugin from '@fullcalendar/daygrid';
 import multiMonthPlugin from '@fullcalendar/multimonth';
 import esLocale from '@fullcalendar/core/locales/es';
+import { useToast } from "vue-toastification"
+import { compileScript } from "vue/compiler-sfc";
 
-//multiMonthYear
+
+let filterSede = null;
+let filterShift = 'm';
+
+const filterPerShift = () => {
+
+    if (filterShift == 'm') {
+        filterShift = "t"
+        useToast().info(
+                "Turno cambiado a tarde"
+            )
+    } else {
+        filterShift = "m"
+        useToast().info(
+                "Turno cambiado a mañana"
+            )
+    }
+    newEvents();
+}
+
+const filterPerSede = () => {
+
+}
 
 const calendarOptions = ref({
     plugins: [dayGridPlugin, multiMonthPlugin],
@@ -14,39 +38,40 @@ const calendarOptions = ref({
     weekends: false,
     events: [],
     dayMaxEventRows: false,
+    customButtons: {
+        filtroTurno: {
+            text: 'Turno',
+            click: filterPerShift,
+        }
+    },
     headerToolbar: {
-    left: 'dayGridMonth,multiMonthYear',
-    center: 'title',
-    right: 'today prev,next'
-  },
+        left: 'dayGridMonth,multiMonthYear filtroTurno',
+        center: 'title',
+        right: 'today prev,next'
+    },
 });
 
-watch(calendarOptions.value.events, (newVal, oldVal) => {
-    if (newVal.length !== oldVal.length) {
-        newEvents();
-    }
-});
 
-const filtroPorTurno = () =>{
-
+const randomNumber = (min, max) => {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-const randomNumber = (min, max) =>{
-    return Math.floor(Math.random() * (max - min + 1) ) + min;
-}
-
-const customColors = () =>{
+const customColors = () => {
     let color = `#${randomNumber(350, 400)}`;
 
     console.log(color)
-    
+
     return color;
 }
 
 const getReservasList = async () => {
     let reservasList = '';
+    console.log(filterShift)
     await axios
-        .get(route('dashboard.reservar.list'))
+        .get(route('dashboard.reservar.list', {
+            turno: filterShift,
+            sede: filterSede
+        }))
         .then((response) => {
             const reserva = response.data.lists
             console.log(reserva)
@@ -55,7 +80,7 @@ const getReservasList = async () => {
                     title: reserva.aula_id,
                     start: reserva.fecha_inicio,
                     end: reserva.fecha_fin,
-                    color  : customColors(),
+                    color: customColors(),
                 }
             });
         })
